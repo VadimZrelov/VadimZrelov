dfde5936c5d346e04a714c2cbbef3255d71cc5df
VadimZrelov <118076239+VadimZrelov@users.noreply.github.com>
2022-12-03 20:31:02 +0400

    Create 12.7.3.py

---
+++ 12.7.3.py
@@ -0,0 +1,8 @@
+money = int(input('Введите сумму вклада, руб.:'))
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+deposit = []
+deposit.append(money*per_cent['ТКБ']/100)
+deposit.append(money*per_cent['СКБ']/100)
+deposit.append(money*per_cent['ВТБ']/100)
+deposit.append(money*per_cent['СБЕР']/100)
+print("Максимальная сумма, которую вы можете заработать:", max(deposit))
